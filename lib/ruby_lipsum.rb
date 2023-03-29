19165fcf48a4eb00cdf4f4e60429e19d47e4e5a0
Ian Drilon <ian.drilon@mashupgarage.com>
2023-03-29 10:31:32 +0800

    feat: adding multiple sentence generator with correct panctuation and capitalization

--- lib/ruby_lipsum.rb
+++ lib/ruby_lipsum.rb
@@ -1,7 +1,21 @@
 class RubyLipsum
+
   def self.generate_words(num)
     words = ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit"]
     result = Array.new(num) { words.sample }
     return result.join(" ")
+  end
+
+  def challenge_multiple_sentence(num)
+    words = ["lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit"]
+    sentences = []
+    num.times do
+      sentence_length = rand(5..15)
+      sentence = Array.new(sentence_length) { words.sample }
+      sentence[0].capitalize!
+      sentence[-1] += "."
+      sentences << sentence.join(" ")
+    end
+    return sentences
   end
 end

ce4826e6d18eb41bfbcc03d81be512a48b7b860f
Joseph <josephvillanueva@github.com>
2023-03-29 10:07:06 +0800

    Task

--- lib/ruby_lipsum.rb
+++ lib/ruby_lipsum.rb
@@ -1,5 +1,8 @@
 class RubyLipsum
-  def self.hello
+  def self.hello(number)
     puts "Hello World"
+    charset = ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit"]
+    Array.new(number) { charset.sample }.join(' ')
   end
+  
 end
